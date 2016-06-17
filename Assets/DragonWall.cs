0f90ff9dcb5d0b3cf911db56b742e9d28f2f3360
Maeda, Kazuya | ASRHQ <kazuya.maeda@rakuten.com>
2016-06-17 13:31:53 +0800

    Create wall

---
+++ Assets/DragonWall.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DragonWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision hit) {
+		if (hit.gameObject.CompareTag ("Player")) {
+			int sceneIndex = SceneManager.GetActiveScene ().buildIndex;
+
+			SceneManager.LoadScene (sceneIndex);
+		}
+	}
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public UnityEngine.UI.Text scoreLabel;
+	public GameObject winnerLabelObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+		int count = GameObject.FindGameObjectsWithTag ("Item").Length;
+		scoreLabel.text = count.ToString ();
+
+		if (count == 0) {
+			winnerLabelObject.SetActive (true);
+		}
+	}
+}
